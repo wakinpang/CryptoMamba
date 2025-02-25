0d4b23ddca9dda2c98344be351087df9a7305950
shahab <sepehri@usc.edu>
2025-02-24 23:31:47 -0800

    new trading strategy

--- utils/trade.py
+++ utils/trade.py
@@ -17,6 +17,24 @@ def buy_sell_smart(today, pred, balance, shares, risk=5):
         balance = 0
     return balance, shares
 
+def buy_sell_smart_w_short(today, pred, balance, shares, risk=5, max_n_btc=0.002):
+    diff = pred * risk / 100
+    if today < pred - diff:
+        shares += balance / today
+        balance = 0
+    elif today < pred:
+        factor = (pred - today) / diff
+        shares += balance * factor / today
+        balance *= (1 - factor)
+    elif today < pred + diff:
+        if shares > 0:
+            factor = (today - pred) / diff
+            balance += shares * factor * today
+            shares *= (1 - factor)
+    else:
+        balance += (shares + max_n_btc) * today
+        shares = -max_n_btc
+    return balance, shares
 
 def buy_sell_vanilla(today, pred, balance, shares, tr=0.01):
     tmp = abs((pred - today) / today)
@@ -40,6 +58,8 @@ def trade(data, time_key, timstamps, targets, preds, balance=100, mode='smart_v2
         assert round(target, 2) == round(data[data[time_key] == int(ts)].iloc[0][y_key], 2)
         if mode == 'smart':
             balance, shares = buy_sell_smart(today, pred, balance, shares, risk=risk)
+        if mode == 'smart_w_short':
+            balance, shares = buy_sell_smart_w_short(today, pred, balance, shares, risk=risk, max_n_btc=0.002)
         elif mode == 'vanilla':
             balance, shares = buy_sell_vanilla(today, pred, balance, shares)
         elif mode == 'no_strategy':
